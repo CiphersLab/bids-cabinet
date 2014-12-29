b73e85edd3b896ba9a17b6a5d05a6be24e9311ce
Muhammad Zubair <fly_zubair@hotmail.com>
2014-12-29 18:43:27 +0500

    Routing work

---
+++ Client/js/Controllers/loginController.js
@@ -0,0 +1,25 @@
+
+
+myApp.controller('loginController',function($scope,$facebook,$state){
+
+    $scope.login = function() {
+        $facebook.login().then(function() {
+            refresh();
+        });
+    }
+    function refresh() {
+        $facebook.api("/me").then(
+            function(response) {
+                $scope.welcomeMsg = "Welcome " + response.name;
+                $scope.isLoggedIn = true;
+                $state.go('ionBarStripped')
+            },
+            function(err) {
+                $scope.welcomeMsg = "Please log in";
+            });
+    }
+
+    refresh();
+
+
+});
--- Client/js/app.js
+++ Client/js/app.js
@@ -3,22 +3,40 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var myApp=angular.module('starter', ['ionic']);
+var myApp=angular.module('starter', ['ionic','ngFacebook']);
 
 myApp.run(['$rootScope', '$window',function($ionicPlatform,$rootScope) {
 
 
-    /*  $ionicPlatform.ready(function() {
-     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-     // for form inputs)
-     if(window.cordova && window.cordova.plugins.Keyboard) {
-     cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-     }
-     if(window.StatusBar) {
-     StatusBar.styleDefault();
-     }
-     }); */
+    (function(){
+        // If we've already installed the SDK, we're done
+        if (document.getElementById('facebook-jssdk')) {return;}
 
+        // Get the first script element, which we'll use to find the parent node
+        var firstScriptElement = document.getElementsByTagName('script')[0];
+
+        // Create a new script element and set its id
+        var facebookJS = document.createElement('script');
+        facebookJS.id = 'facebook-jssdk';
+
+        // Set the new script's source to the source of the Facebook JS SDK
+        facebookJS.src = 'http://connect.facebook.net/en_US/all.js';
+
+        // Insert the Facebook JS SDK into the DOM
+        firstScriptElement.parentNode.insertBefore(facebookJS, firstScriptElement);
+    }());
+
+  /*  $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if(window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if(window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+*/
 
 
 }]);
@@ -29,43 +47,34 @@ myApp.config(function($facebookProvider,$stateProvider, $urlRouterProvider) {
     $facebookProvider.setVersion("v2.2");
 
 
-    $urlRouterProvider.otherwise('/')
+    $urlRouterProvider.otherwise('/');
+
     $stateProvider.state('home', {
         url: '/',
         views: {
             home: {
-                templateUrl: 'templates/home.html'
+                templateUrl: 'templates/home.html',
+                controller:'loginController'
             }
         }
     })
 
-    $stateProvider.state('help', {
-        url: '/help',
-        views: {
-            help: {
-                templateUrl: ''
+        .state('help', {
+            url: '/help',
+            views: {
+                help: {
+                    templateUrl: ''
+                }
             }
-        }
-    })
-
-    /*$stateProvider
-
+        })
 
         .state('ionBarStripped',{
 
 
-            abstract:true,
-            templateUrl:"ionBarStriped.html"
+            /*abstract:true,*/
+            templateUrl:"templates/ionBarStriped.html"
         })
-        .state('home',{
-            url:'/',
-            views:{
-                home:{
-                    templateUrl:"templates/home.html",
-                    controller: 'loginController'
-                }
-            }
-        })
+
         .state('ionBarStripped.yourTeam',{
 
 
@@ -137,7 +146,6 @@ myApp.config(function($facebookProvider,$stateProvider, $urlRouterProvider) {
 
 
 
-    $urlRouterProvider.otherwise("/");*/
 
 
 });
